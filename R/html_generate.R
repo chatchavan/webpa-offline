1222fe2ad90150b656d2ea4962157a086e17ce13
Chat Wacharamanotham <chat@ifi.uzh.ch>
2026-05-26 09:11:40 +0200

    fix team ID quoting

--- R/html_generate.R
+++ R/html_generate.R
@@ -16,7 +16,7 @@ generate_html <- function(team_df){
   html_content <- 
     html_template |> 
     str_replace(coll('let teamId = 1;'), 
-                paste0('let teamId = ', teamId, ';')) |> 
+                paste0('let teamId = "', teamId, '";')) |> 
     str_replace(coll('let students = ["John Doe", "Jane Smith", "Sam Johnson", "Michael Brown"];'), 
                 paste0('let students = ', jsonlite::toJSON(students), ';')) |> 
     str_replace(coll('let uploadInstruction = null;'), 
